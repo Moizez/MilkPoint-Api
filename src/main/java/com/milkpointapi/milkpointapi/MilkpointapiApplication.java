657ddba2eda507882b1ad00cf3c2ece3b54adc74
Carlos Moisés B. Henrique <moizezhenrique@gmail.com>
2020-01-03 08:22:50 -0300

    Add files via upload
    Commit inicial

---
+++ src/main/java/com/milkpointapi/milkpointapi/MilkpointapiApplication.java
@@ -0,0 +1,13 @@
+package com.milkpointapi.milkpointapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkpointapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkpointapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milkpointapi/milkpointapi/MilkpointapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milkpointapi.milkpointapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkpointapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
