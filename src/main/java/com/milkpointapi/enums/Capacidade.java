65117e1865abf38e1dce89180928a3db46b2731e
leandrogrego <contato@leandrorego.com>
2020-02-09 12:39:30 -0300

    Gráfico do tanque
    corresção de gráfico do tanque

--- src/main/java/com/milkpointapi/enums/Capacidade.java
+++ src/main/java/com/milkpointapi/enums/Capacidade.java
@@ -4,14 +4,17 @@ public enum Capacidade {
 	MIL("1000"), DOISMIL("2000"), TRESMIL("3000"), QUATROMIL("4000"),
 	QUATROMILEQUINHENTOS("4500");
 
-	private String descricao;
+	private String valor;
 
-	Capacidade(String descricao) {
-		this.descricao = descricao;
+	Capacidade(String valor) {
+		this.valor = valor;
+	}
+
+	public String getValor() {
+		return valor;
 	}
 	
-
 	public String getDescricao() {
-		return descricao;
+		return valor + " Litros";
 	}
 }
