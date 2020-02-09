3090c9e21c55a5a833459a239118a8cc4c37fd39
leandrogrego <contato@leandrorego.com>
2020-02-09 11:51:17 -0300

    Gráfico do Tamque
    Ilustração do tanque corrigida

--- src/main/java/com/milkpointapi/enums/Capacidade.java
+++ src/main/java/com/milkpointapi/enums/Capacidade.java
@@ -1,14 +1,15 @@
 package com.milkpointapi.enums;
 
 public enum Capacidade {
-	MIL("1000 Litros"), DOISMIL("2000 Litros"), TRESMIL("3000 Litros"), QUATROMIL("4000 Litros"),
-	QUATROMILEQUINHENTOS("4500 Litros");
+	MIL("1000"), DOISMIL("2000"), TRESMIL("3000"), QUATROMIL("4000"),
+	QUATROMILEQUINHENTOS("4500");
 
 	private String descricao;
 
 	Capacidade(String descricao) {
 		this.descricao = descricao;
 	}
+	
 
 	public String getDescricao() {
 		return descricao;
