c5d27e2465766e4d2635497be77a735aa18d907d
Carlos Moisés B. Henrique <moizezhenrique@gmail.com>
2020-06-16 20:17:59 -0300

    Update ResponsavelResource.java
    desfiz mudanças no nome tanque

--- src/main/java/com/milkpointapi/api/ResponsavelResource.java
+++ src/main/java/com/milkpointapi/api/ResponsavelResource.java
@@ -54,7 +54,7 @@ public class ResponsavelResource {
 		return ResponseEntity.ok(responsavel);
 	}
 	
-	@GetMapping("/responsavel/{id}/tanque")
+	@GetMapping("/responsavel/{id}/tanques")
 	public ResponseEntity<List<Tanque>> tanques(@PathVariable Long id) {
 		Responsavel responsavel = responsavelService.findOne(id);
 		if (responsavel == null) {
